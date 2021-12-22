05bcb30a917a2728fdef1739e08d1849316cb8ae
NuSa <lafon.eddy@gmail.com>
2021-12-22 10:09:00 +0100

    require

--- soraredata_sorare.js
+++ soraredata_sorare.js
@@ -1,6 +1,8 @@
 // event when page is load
 window.addEventListener("load", getPlayer);
 
+console.log("SorareStats");
+
 // event when url change
 let lastUrl = location.href; 
 new MutationObserver(() => {
